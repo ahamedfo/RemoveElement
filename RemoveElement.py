5a61f147afd73ce9bb9b5e2319948610065f91f2
ahamedfofana <ahamedfofana12@gmail.com>
2020-05-06 17:59:10 -0400

    Runtime: 24 ms, faster than 37.74% of Python online submissions for Remove Element.
    Memory Usage: 12.7 MB, less than 5.66% of Python online submissions for Remove Element.

--- RemoveElement.py
+++ RemoveElement.py
@@ -1,5 +1,13 @@
 class Solution(object):
     def removeElement(self, nums, val):
+        k = 0
+        idx = 0
+        for number in nums:
+            if number != val:
+                nums[idx] = number
+                k+= 1
+                idx += 1
+        return k
         """
         :type nums: List[int]
         :type val: int
